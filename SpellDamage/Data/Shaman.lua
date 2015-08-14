439acac8ebcfa5ab3b031254f4716205b6295f21
Demonist <Demonist616@gmail.com>
2015-08-14 05:46:13 +0300

    0.8.5.6 Подправлено заклинание "Земной шок".

--- SpellDamage/Data/Shaman.lua
+++ SpellDamage/Data/Shaman.lua
@@ -1,3 +1,16 @@
+--Земной шок:
+local EarthShock = MultiParser:create(SpellDamage, {1}, function(data, match)
+	data.damage = match[1]
+
+	local name, _, _, count = UnitBuff("player", "Щит молний")
+	if IsSpellKnown(88766) == true and name and count > 1 then	--Сверкание
+		local damage = matchDigit(GetSpellDescription(324), 1)	--Щит молний
+		if damage then
+			data.damage = match[1] + (damage * (count - 1))
+		end
+	end
+end)
+
 --Удар бури:
 local Stormstrike = MultiParser:create(SpellDamage, {1, 2}, function(data, match)
 	data.damage = match[1] + match[2]
@@ -21,7 +34,7 @@ end)
 Shaman = Class:create(ClassSpells)
 Shaman.spells[403]		= SimpleDamageParser 								--Молния
 Shaman.spells[73899]	= SimpleDamageParser 								--Стихийный удар
-Shaman.spells[8042]		= SimpleDamageParser 								--Земной шок
+Shaman.spells[8042]		= EarthShock 										--Земной шок
 Shaman.spells[8004]		= SimpleHealParser 									--Исцеляющий всплеск
 Shaman.spells[61295]	= DoubleParser:create(SpellHealAndTimeHeal, 1, 2) 	--Быстрина
 Shaman.spells[60103]	= SimpleDamageParser 								--Вскипание лавы
