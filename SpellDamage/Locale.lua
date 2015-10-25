f567a9ca0477db339dd190cc69d144eac9d1346c
Demonist <Demonist616@gmail.com>
2015-10-25 13:48:44 +0300

    Чернокнижник.

--- SpellDamage/Locale.lua
+++ SpellDamage/Locale.lua
@@ -5,6 +5,7 @@ local locale = GetLocale()
 L["daggers"] = "Daggers"
 L["lightning_shield"] = "Lightning Shield"
 L["frost_bomb"] = "Frost Bomb"
+L["haunt"] = "Haunt"
 
 --------------------------------   ruRU   -------------------------------------
 
@@ -12,6 +13,7 @@ if locale == "ruRU" then
 	L["daggers"] = "Кинжалы"
 	L["lightning_shield"] = "Щит молний"
 	L["frost_bomb"] = "Ледяная бомба"
+	L["haunt"] = "Блуждающий дух"
 	return
 end
 
@@ -21,6 +23,7 @@ if locale == "deDE" then
 	L["daggers"] = "Dolche"
 	L["lightning_shield"] = "Blitzschlagschild"
 	L["frost_bomb"] = "Frostbombe"
+	L["haunt"] = "Heimsuchung"
 	return
 end
 
@@ -30,6 +33,7 @@ if locale == "esES" then
 	L["daggers"] = "Dagas"
 	L["lightning_shield"] = "Escudo de relámpagos"
 	L["frost_bomb"] = "Bomba de Escarcha"
+	L["haunt"] = "Poseer"
 	return
 end
 
@@ -39,6 +43,7 @@ if locale == "frFR" then
 	L["daggers"] = "Dagues"
 	L["lightning_shield"] = "Bouclier de foudre"
 	L["frost_bomb"] = "Bombe de givre"
+	L["haunt"] = "Hanter"
 	return
 end
 
@@ -48,6 +53,7 @@ if locale == "itIT" then
 	L["daggers"] = "Pugnali"
 	L["lightning_shield"] = "Scudo di Fulmini"
 	L["frost_bomb"] = "Bomba del Gelo"
+	L["haunt"] = "Tormento"
 	return
 end
 
@@ -57,6 +63,7 @@ if locale == "ptBR" then
 	L["daggers"] = "Daggers"
 	L["lightning_shield"] = "Escudo de Raios"
 	L["frost_bomb"] = "Bomba Gélida"
+	L["haunt"] = "Possessão"
 	return
 end
 
@@ -66,6 +73,7 @@ if locale == "zhCN" then
 	L["daggers"] = "匕首"
 	L["lightning_shield"] = "闪电之盾"
 	L["frost_bomb"] = "寒冰炸弹"
+	L["haunt"] = "鬼影缠身"
 	return
 end
 
